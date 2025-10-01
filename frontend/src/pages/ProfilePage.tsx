1d618be7e36af626d588626544bc47cf9cc16066
danielleconeva <dconewa@gmail.com>
2025-10-01 22:52:04 +0300

    Added ProfilePage container with section structure

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -1,3 +1,35 @@
+import ProfileHeader from "../components/profile/ProfileHeader";
+import PersonalInfo from "../components/profile/PersonalInfo";
+import MyRides from "../components/profile/MyRides";
+import CarDetails from "../components/profile/CarDetails";
+import RideHistory from "../components/profile/RideHistory";
+import Preferences from "../components/profile/Preferences";
+import styled from "styled-components";
+
+const Wrap = styled.div`
+    max-width: 1100px;
+    margin: 0 auto;
+    padding: 32px 16px;
+`;
+
+const SectionList = styled.div`
+    margin-top: 32px;
+    display: flex;
+    flex-direction: column;
+    gap: 48px;
+`;
+
 export default function ProfilePage() {
-    return <></>;
+    return (
+        <Wrap>
+            <ProfileHeader />
+            <SectionList>
+                <PersonalInfo />
+                <MyRides />
+                <CarDetails />
+                <RideHistory />
+                <Preferences />
+            </SectionList>
+        </Wrap>
+    );
 }
